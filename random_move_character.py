32d81789ecbf76457c92db102ad30d2ff1812bf0
2021182046 <lope32@naver.com>
2023-10-01 16:55:06 +0900

    Import lib, png

---
+++ random_move_character.py
@@ -0,0 +1,10 @@
+from pico2d import *
+import random
+
+open_canvas()
+character = load_image('animation_sheet.png')
+tuk_ground = load_image('TUK_GROUND.png')
+mouse = load_image('hand_arrow.png')
+
+
+
