a30804da3d56ec2540a6950de501227faf8a1cb0
2021182046 <lope32@naver.com>
2023-10-01 18:06:30 +0900

    Update move_character_2
    랜덤으로 표시된 포인트로 character가 이동하도록 수정

--- random_move_character.py
+++ random_move_character.py
@@ -15,20 +15,30 @@ def draw_line(p1, p2):
         t = i/100
         x = (1-t)*x1 + t*x2
         y = (1-t)*y1 + t*y2
+
+        clear_canvas()
+        tuk_ground.draw(800//2, 600//2)
+        for p in points:
+            #clear_canvas()
+            #tuk_ground.draw_now(800// , 600//2)
+            mouse.draw(p[0], p[1])
         character.clip_draw(frame * 100, 0, 100, 100, x, y)
+        update_canvas()
+        delay(0.1)
 
 while True:
-    #clear_canvas()
-    #tuk_ground.draw_now(800//2, 600//2)
+    clear_canvas()
+    tuk_ground.draw_now(800//2, 600//2)
 
     points = [ (random.randint(0, 800) , random.randint(0,600)) for i in range(10) ]
-    for p in points:
+    '''for p in points:
         clear_canvas()
         tuk_ground.draw_now(800//2, 600//2)
         mouse.draw(p[0], p[1])
-
+    '''
     for i in range(0, len(points) - 1):
         draw_line(points[i], points[i + 1])
+        frame = (frame + 1) % 8
 
     update_canvas()
     delay(0.1)
