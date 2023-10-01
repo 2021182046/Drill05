7d4e21231e21f7bc57ac8a64897b525f3f395986
2021182046 <lope32@naver.com>
2023-10-01 17:08:24 +0900

    Update random_point hand
    List Comprehension으로 10개 포인트 리스트 생성 및 mouse 생성

--- random_move_character.py
+++ random_move_character.py
@@ -6,5 +6,15 @@ character = load_image('animation_sheet.png')
 tuk_ground = load_image('TUK_GROUND.png')
 mouse = load_image('hand_arrow.png')
 
+while True:
+    #clear_canvas()
+    #tuk_ground.draw_now(800//2, 600//2)
 
+    points = [ (random.randint(0, 800) , random.randint(0,600)) for i in range(10) ]
+    for p in points:
+        clear_canvas()
+        tuk_ground.draw_now(800//2, 600//2)
+        mouse.draw(p[0], p[1])
+    update_canvas()
+    delay(0.1)
 
