65d01d8bca461007ed56778f19f99d4b519ae964
2021182046 <lope32@naver.com>
2023-10-01 17:30:17 +0900

    Add move_character(미완)
    선분이동 함수를 이용해 랜덤포인트로 이동, 한번에 표시되는 문제 해결중

--- random_move_character.py
+++ random_move_character.py
@@ -5,6 +5,7 @@ open_canvas()
 character = load_image('animation_sheet.png')
 tuk_ground = load_image('TUK_GROUND.png')
 mouse = load_image('hand_arrow.png')
+frame = 0
 
 def draw_line(p1, p2):
     x1, y1 = p1[0], p1[1]
@@ -14,6 +15,7 @@ def draw_line(p1, p2):
         t = i/100
         x = (1-t)*x1 + t*x2
         y = (1-t)*y1 + t*y2
+        character.clip_draw(frame * 100, 0, 100, 100, x, y)
 
 while True:
     #clear_canvas()
@@ -24,6 +26,10 @@ while True:
         clear_canvas()
         tuk_ground.draw_now(800//2, 600//2)
         mouse.draw(p[0], p[1])
+
+    for i in range(0, len(points) - 1):
+        draw_line(points[i], points[i + 1])
+
     update_canvas()
     delay(0.1)
 
