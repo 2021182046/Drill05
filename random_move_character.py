bd8859ed115f79828ef3245296f9861e5b9039b8
2021182046 <lope32@naver.com>
2023-10-01 17:19:43 +0900

    Add draw_line
    Parametric Representation을 이용한 선분이동 함수 draw_line 추가

--- random_move_character.py
+++ random_move_character.py
@@ -6,6 +6,15 @@ character = load_image('animation_sheet.png')
 tuk_ground = load_image('TUK_GROUND.png')
 mouse = load_image('hand_arrow.png')
 
+def draw_line(p1, p2):
+    x1, y1 = p1[0], p1[1]
+    x2, y2 = p2[0], p2[1]
+
+    for i in range(0, 100+1, 5):
+        t = i/100
+        x = (1-t)*x1 + t*x2
+        y = (1-t)*y1 + t*y2
+
 while True:
     #clear_canvas()
     #tuk_ground.draw_now(800//2, 600//2)
