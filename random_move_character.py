4c8099b044d1d6e053008bb62b33e882645e3b28
2021182046 <lope32@naver.com>
2023-10-01 18:32:44 +0900

    Update move_character_3
    이동해야 할 포인트 p2에만 mouse를 표시하도록 수정하고 코드 정리

--- random_move_character.py
+++ random_move_character.py
@@ -16,12 +16,10 @@ def draw_line(p1, p2):
         x = (1-t)*x1 + t*x2
         y = (1-t)*y1 + t*y2
 
-        clear_canvas()
-        tuk_ground.draw(800//2, 600//2)
         for p in points:
-            #clear_canvas()
-            #tuk_ground.draw_now(800// , 600//2)
-            mouse.draw(p[0], p[1])
+            clear_canvas()
+            tuk_ground.draw(800 // 2, 600 // 2)
+            mouse.draw(p2[0], p2[1])
         character.clip_draw(frame * 100, 0, 100, 100, x, y)
         update_canvas()
         delay(0.1)
@@ -31,11 +29,7 @@ while True:
     tuk_ground.draw_now(800//2, 600//2)
 
     points = [ (random.randint(0, 800) , random.randint(0,600)) for i in range(10) ]
-    '''for p in points:
-        clear_canvas()
-        tuk_ground.draw_now(800//2, 600//2)
-        mouse.draw(p[0], p[1])
-    '''
+
     for i in range(0, len(points) - 1):
         draw_line(points[i], points[i + 1])
         frame = (frame + 1) % 8
